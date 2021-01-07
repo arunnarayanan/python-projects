ac224b5786cd9ad28689a5c349f17c671ed54589
Arun Narayanan <arun.narayanan@hummingbiz.com>
2021-01-07 14:07:17 -0600

    Added user authentication and user account creation. Added account route that is available ONLY for a logged in user. ** Added flask-login module to assist with user login/logout features **

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 # Used secrets.token_hex() to generate this token
 app.config['SECRET_KEY'] = '87e5bef19cad3e5b24b888d25653d79a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
