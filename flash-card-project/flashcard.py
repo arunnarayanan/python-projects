cdf30829418910fc6c1bef769b43f0af8f52f2b6
Arun Narayanan <arun.nara@gmail.com>
2022-12-31 00:10:53 -0600

    Flashcard final version - saves 'to learn' words

--- flash-card-project/flashcard.py
+++ flash-card-project/flashcard.py
@@ -4,7 +4,12 @@ import pandas as pd
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-words_df = pd.read_csv('data/french_words.csv')
+try:
+    words_df = pd.read_csv('data/words_to_learn.csv')
+except FileNotFoundError:
+    print('words_to_learn.csv not found. Using the base file')
+    words_df = pd.read_csv('data/french_words.csv')
+
 words_dict_list = words_df.to_dict(orient="records")
 current_word = {}
 
@@ -19,9 +24,17 @@ def next_card():
     flip_timer = root.after(3000, flip_card)
 
 def flip_card():
+    global current_word
     canvas.itemconfig(canvas_image, image=back_img)
     canvas.itemconfigure(word_txt, text=f'{current_word["English"]}', fill="white")
     canvas.itemconfigure(lang_txt, text='English', fill="white")
+
+def remove_card():
+    print(f'Removing {current_word} from the list')
+    words_dict_list.remove(current_word)
+    pd.DataFrame(words_dict_list).to_csv('data/words_to_learn.csv', index=False)
+    next_card()
+
 
 root = Tk()
 root.title('Flashcard')
@@ -39,11 +52,11 @@ word_txt = canvas.create_text(400, 253, text='', font=("Ariel", 60, "bold"))
 canvas.grid(row=0, columnspan=2)
 
 wrong_img = PhotoImage(file='images/wrong.png')
-wrong_btn = Button(root, image=wrong_img, highlightthickness=0, command=next_card, )
+wrong_btn = Button(root, image=wrong_img, highlightthickness=0, command=next_card)
 wrong_btn.grid(row=1)
 
 right_img = PhotoImage(file='images/right.png')
-right_btn = Button(root, image=right_img, highlightthickness=0, command=next_card)
+right_btn = Button(root, image=right_img, highlightthickness=0, command=remove_card)
 right_btn.grid(row=1, column=1)
 
 next_card()
